1492ce792e3c343010dbd82984b69cc328eab388
Sergey_Chernyaev <schernyae@gmail.com>
2018-04-16 15:46:19 +0300

    [FEATURE] Запрет на одновременный запуск двух версий приложения

--- HakutakuDesktop/Program.cs
+++ HakutakuDesktop/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace HakutakuDesktop
@@ -11,18 +12,29 @@ namespace HakutakuDesktop
 		public static OverlayForm _mainForm;
 		public static SelectionForm _selectionForm;
 
+		private static string appGuid = "hakutaku-desktop-app";
+
 		[STAThread]
 		static void Main()
 		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			_mainForm = new OverlayForm();
-			_selectionForm = new SelectionForm();
-			_selectionForm.Owner = _mainForm;
-			InterceptKeys.SetCallback(HotKey);
-			Logger.WriteLog("Launching app");
-			Application.Run(_mainForm);
-			InterceptKeys.RemoveCallback();
+			using (Mutex mutex = new Mutex(false, @"Global\" + appGuid))
+			{
+				if (!mutex.WaitOne(0, false))
+				{
+					MessageBox.Show("Instance already running. Press F7 to display overlay. Press F6 to exit app.");
+					return;
+				}
+				GC.Collect();
+				Application.EnableVisualStyles();
+				Application.SetCompatibleTextRenderingDefault(false);
+				_mainForm = new OverlayForm();
+				_selectionForm = new SelectionForm();
+				_selectionForm.Owner = _mainForm;
+				InterceptKeys.SetCallback(HotKey);
+				Logger.WriteLog("Launching app");
+				Application.Run(_mainForm);
+				InterceptKeys.RemoveCallback();
+			}
 		}
 
 		private static void HotKey(Keys key)
